230411ba07faf7522c33fd10a6ff9b728f260fe3
sennhang <shengnan96@live.com>
2023-05-13 00:35:56 +1000

    some change

--- Assignment2ansible/run-mrc.sh
+++ Assignment2ansible/run-mrc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ansible-galaxy collection install openstack.cloud:2.0.0
-. /Users/sennhang/Documents/CCC/A2/pt-74505-openrc.sh; ansible-playbook -vv mrc.yaml | tee output.txt
+. /unimelb-comp90024-2023-grp-25-openrc.sh; ansible-playbook -vv mrc.yaml | tee output.txt
